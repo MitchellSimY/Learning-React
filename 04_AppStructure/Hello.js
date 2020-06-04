63366f1fae30f40d98007c94b825aa9c60d821c2
MitchellSimY <Mitchellsimy@gmail.com>
2020-06-04 15:33:55 -0500

    Third commit from Laptop.

--- 04_AppStructure/Hello.js
+++ 04_AppStructure/Hello.js
@@ -1,5 +1,5 @@
 class Hello extends React.Component {
     render() {
-        return <h1>Hello World! This is a component. This commit is from my laptop. </h1>
+        return <h1>Hello World! This is a component. This commit is from my laptop . </h1>
     }
 }
