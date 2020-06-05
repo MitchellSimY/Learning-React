5ce6a6c1a519cb22a9b40060d126c82f3a9e2052
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-06-05 00:50:30 -0500

    Section 5.

--- 03_Props/2_SlotsExercise/SlotMachine/Machine.js
+++ 03_Props/2_SlotsExercise/SlotMachine/Machine.js
@@ -1,21 +1,18 @@
 
-function isEqual (s1,s2,s3) {
-    if (s1 === s2 && s1 === s3) {
-        return "You win!";
-    } else {
-        return "Did not match.";
-    }
-}
 
 
 class Machine extends React.Component {
     render() {
-        const colors = { fontSize: '10px', backgroundColor: 'green' }
+        const {s1, s2, s3} = this.props;
+        const winner = s1 === s2 & s2 === s3;
+
+
+        const colors = { fontSize: '10px'}
         return (
             <div className="Machine">
                 <p style={colors}>
                 <h1>{this.props.s1} {this.props.s2} {this.props.s3}</h1>
-                <h2>{isEqual(this.props.s1,this.props.s2,this.props.s3)}</h2>
+                <h2 className={winner ? 'win' : 'lose'}>{winner ? 'Winner!' : 'Loser!'}</h2>
                 </p>
             </div>
         )
