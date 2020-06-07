85950f0d7f085d8d4bfbbaad78c92b9760b9a14c
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-06-06 23:53:40 -0500

    CRA Assets

---
+++ 05_CreateReactApp/cra_conventions/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Dog from './Dog'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Dog />
+
+    </div>
+  );
+}
+
+export default App;
