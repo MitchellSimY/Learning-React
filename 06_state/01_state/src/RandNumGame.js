51d37f24cef940c7309df8f893c860a26b3b9b52
MitchellSimY <Mitchellsimy@gmail.com>
2020-06-11 14:46:07 -0500

    RNG Game commit.

--- 06_state/01_state/src/RandNumGame.js
+++ 06_state/01_state/src/RandNumGame.js
@@ -1,13 +1,29 @@
-import React, { Component } from "./React";
+import React, { Component } from "react";
 
 class RandNumGame extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            rng: Math.floor(Math.random() * 10)
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    handleClick() {
+        console.log("click");
+        let rand = Math.floor(Math.random() * 10);
+        this.setState({ rng: rand });
+    }
 
 
     render() {
         return (
             <div>
-                <button>Click to change number!</button>
+                <h1>{this.state.rng}</h1>
+                <h2>{this.state.rng === 7  || this.state.rng === 3 ? "You win!" : this.state.rng}</h2>
+                <button onClick={this.handleClick}>Click to change number!</button>
             </div>
         )
     }
