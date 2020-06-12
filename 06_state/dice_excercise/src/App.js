d705aee98b5a46cdf46b4b12ca28728e59fe20f2
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-06-12 18:06:54 -0500

    Dice exercise.

---
+++ 06_state/dice_excercise/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ButtonRoll from './ButtonRoll.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonRoll />
+    </div>
+  );
+}
+
+export default App;
