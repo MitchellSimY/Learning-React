2503490be7b59a74f7b73dffe681932d22ddcfd8
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-07-05 15:43:31 -0500

    Forms

---
+++ 10_forms/01_firstform/src/Form.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "Null"
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(evt) {
+        this.setState({ username: evt.target.value })
+    }
+
+    handleSubmit(evt) {
+        console.log("tst");
+        alert(`You typed: ${this.state.username}`)
+        this.setState({username: ""})
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Form demo</h1>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text"
+                        value={this.state.username}
+                        onChange={this.handleChange} />
+
+                    <button>Submit!</button>
+                </form>
+
+                <p>Name is: {this.state.username}</p>
+
+
+            </div>
+        )
+    }
+}
+
+export default Form;
