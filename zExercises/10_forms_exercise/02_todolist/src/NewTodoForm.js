f50d51d529213f50e0fe1e582898135d4d6a6ba0
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-07-10 11:24:22 -0500

    Installed uuid

--- zExercises/10_forms_exercise/02_todolist/src/NewTodoForm.js
+++ zExercises/10_forms_exercise/02_todolist/src/NewTodoForm.js
@@ -1,12 +1,37 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class NewTodoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todo: ""
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+
+        this.props.addListFunction(this.state);
+    }
+
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+
+
     render() {
-        return(
-            <form>
-                <input type = "text" 
-                name="todo"
-                /> 
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>New Todo!</label> <br></br>
+                <input type="text"
+                    name="todo"
+                    value={this.state.todo}
+                    onChange={this.handleChange}
+                />
                 <button>Add</button>
             </form>
         )
--- zExercises/10_forms_exercise/02_todolist/src/TodoList.js
+++ zExercises/10_forms_exercise/02_todolist/src/TodoList.js
@@ -2,11 +2,32 @@ import React, {Component} from "react";
 import NewToDoForm from "./NewTodoForm";
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todoList: []
+        };
+
+        this.addList = this.addList.bind(this);
+    }
+
+
+    addList(list) {
+        // console.log(`Adding todo! ${list.todo}`);
+        
+    
+    }
+
+    renderList() {
+
+    }
+
     render() {
         return( 
             <div>
                 <p>TodoList Component</p>
-                <NewToDoForm />
+                <NewToDoForm addListFunction={this.addList}/>
             </div>
         )
     }
