cdfec9fb12e681b01ee0ca7d61fad349144e1ae9
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-07-21 18:33:53 -0500

    Component Did mount.

--- 11_lifecycles/01_timer/src/Timer.js
+++ 11_lifecycles/01_timer/src/Timer.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 
-class Timer extends Component () {
-    render () {
+class Timer extends Component {
+    constructor(props) {
+        super(props);
+        console.log("In constructor!");
+    }
+
+    componentDidMount() {
+        console.log("In component did mount.");
+    }
+
+    render() {
         return (
-            <div>Test!</div>
+            <div>
+                <h1>test</h1>
+            </div>
         )
     }
 }
