8e2105771c7aaa61daf39e16aa048a5b4f92d92a
MitchellSimY <Mitchellsimy@gmail.com>
2020-07-29 14:29:17 -0500

    finished github user info.

--- 11_lifecycles/01_timer/src/GithubUserInfo.js
+++ 11_lifecycles/01_timer/src/GithubUserInfo.js
@@ -4,20 +4,23 @@ import axios from "axios";
 class GithubUserInfo extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { imgUrl: "", name: "" };
     }
 
+    // watch video on async 
     async componentDidMount() {
         const url = `https://api.github.com/users/${this.props.username}`;
-        console.log(url);
         let response = await axios.get(url);
-        console.log(response);
+        let data = response.data;
+        console.log(data);
+        this.setState({ imgUrl: data.avatar_url, name: data.name })
     }
 
     render() {
         return (
             <div>
-                <h1>GITHUB USER INFO</h1>
+                <h1>GITHUB USER: {this.state.name}</h1>
+                <img src={this.state.imgUrl}></img>
             </div>
         )
     }
