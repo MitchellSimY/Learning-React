b0c0cc78dbc00915a170600faed4a60c4b1e5a62
MitchellSimY <Mitchellsimy@gmail.com>
2020-07-29 13:14:49 -0500

    Asynch functions

--- 11_lifecycles/01_timer/src/App.js
+++ 11_lifecycles/01_timer/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Timer from './Timer';
 import ZenQuote from "./ZenQuote";
+import GithubUserInfo from "./GithubUserInfo"
 
 function App() {
   return (
     <div className="App">
       {/* <Timer /> */}
-      <ZenQuote />
+      {/* <ZenQuote /> */}
+      <GithubUserInfo username="Mitchelll" />
     </div>
   );
 }
