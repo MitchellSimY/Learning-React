2ed1f64900ae1174f597cc4e1ef2a0957ca917a8
MitchellSimY <Mitchellsimy@gmail.com>
2020-07-29 15:55:58 -0500

    Deck made.

---
+++ zExercises/011_lifeCycles/deckofcards/src/Deck.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+class Deck extends Component {
+    constructor(props) {
+        super(props);
+        this.state
+    }
+
+    componentDidMount() {
+        const deckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/';
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Test</h1>
+            </div>
+        )
+    }
+}
+
+export default Deck;
