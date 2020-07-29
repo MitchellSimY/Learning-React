5916e82958049c0051efa0e87f468b090a7aa70a
MitchellSimY <Mitchellsimy@gmail.com>
2020-07-29 13:05:57 -0500

    zen quote completed.

--- 11_lifecycles/01_timer/src/ZenQuote.js
+++ 11_lifecycles/01_timer/src/ZenQuote.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class ZenQuote extends Component {
+    constructor(props) {
+        super(props);
+        // Always initialize it into something.
+        // Without this - there would be errors.
+        // Why? Because the componentDidMount is rednered afterwards.   
+        this.state = {quote: ""}
+    }
+
     componentDidMount () {
         //load data
         axios.get("https://api.github.com/zen").then(response => {
