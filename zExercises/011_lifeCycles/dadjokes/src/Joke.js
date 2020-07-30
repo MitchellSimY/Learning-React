50155ef51cae2dd9abe06030e9b737602398b6be
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-07-30 11:12:31 -0500

    Joke component created. Mapped for output

---
+++ zExercises/011_lifeCycles/dadjokes/src/Joke.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class Joke extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render () {
+        return (
+            <div>
+                <h1>From Joke Component</h1>
+            </div>
+        )
+    }
+}
+
+export default Joke;
--- zExercises/011_lifeCycles/dadjokes/src/JokesList.js
+++ zExercises/011_lifeCycles/dadjokes/src/JokesList.js
@@ -1,26 +1,41 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Joke from "./Joke"
 
 const API_URL = 'https://icanhazdadjoke.com/';
 
 class JokesList extends Component {
     constructor(props) {
         super(props);
-        this.state = { jokeInfo: [] }
+        this.state = { jokes: [] }
     }
 
     async componentDidMount() {
-        let getJoke = await axios.get(API_URL, { headers: { Accept: "application/json" }});
-        console.log(getJoke);
+        // Getting the joke data.
+        let i = 0;
+        do {
+            let getJoke = await axios.get(API_URL, { headers: { Accept: "application/json" } });
+            let jokeData = getJoke.data;
+
+            let score = 0;
+            let jokeInfo = [jokeData, score];
+            this.setState(st => ({
+                jokes: [...this.state.jokes, jokeInfo]
+            }))
+            i++
+        } while (i != 10)
+        console.log(this.state.jokes);
 
     }
 
 
-
     render() {
+
+        const jokeOutput = this.state.jokes.map(jokes => <Joke jokeInformation={jokes}/>);
         return (
             <div>
                 <h1>Test</h1>
+                {jokeOutput}
             </div>
         )
     }
