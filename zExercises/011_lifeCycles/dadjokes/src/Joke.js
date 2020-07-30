4dfe0378acd8ab68dfc34017010aa17689b20694
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-07-30 10:40:13 -0500

    Npm installed axios.

--- zExercises/011_lifeCycles/dadjokes/src/Joke.js
+++ zExercises/011_lifeCycles/dadjokes/src/Joke.js
@@ -1,11 +1,20 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import axios from "axios";
+
+const API_URL = 'https://icanhazdadjoke.com/';
 
 class Joke extends Component {
     constructor(props) {
         super(props);
+        this.state = { jokeInfo: [] }
     }
 
-    
+    async componentDidMount() {
+        let getJoke = await axios.get(API_URL);
+        this.setState({jokeInfo: [...getJoke]})
+    }
+
+
 
     render() {
         return (
