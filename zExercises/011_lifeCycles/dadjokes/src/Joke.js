7a2b9c7f005aff56701936a50777558e36a820de
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-08-04 14:38:13 -0500

    Copied fontawesome from colt.

--- zExercises/011_lifeCycles/dadjokes/src/Joke.js
+++ zExercises/011_lifeCycles/dadjokes/src/Joke.js
@@ -6,17 +6,13 @@ class Joke extends Component {
         super(props);
     }
 
-
-    // Give upvote and downvote button.
-    // Maybe pass upwards?
-
     render() {
 
         return (
             <div className="Joke">
                 <div className="Joke-buttons">
                     <i className="fas fa-arrow-up"></i>
-                    <span>{this.props.vote}</span>
+                    <span>{this.props.votes}</span>
                     <i className="fas fa-arrow-down"></i>
                 </div>
                 <div className="Joke-text">{this.props.text}</div>
