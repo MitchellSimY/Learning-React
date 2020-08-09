ba018228457a13b392874e9dcb72be8bed3f8756
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-08-09 12:07:37 -0500

    made dog, about and contact JS files.

---
+++ 12_reactrouters/01_routerintro/src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Test from About</h2>
+            </div>
+        )
+    }
+}
+
+export default About;
---
+++ 12_reactrouters/01_routerintro/src/Contact.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Test from contact</h2>
+            </div>
+        )
+    }
+}
+
+export default Contact;
