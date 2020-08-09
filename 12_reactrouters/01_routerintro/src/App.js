8eae9d51c1e2401d0ceda5ce154bfc540b7ab171
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-08-09 12:58:44 -0500

    Link component added.

--- 12_reactrouters/01_routerintro/src/App.js
+++ 12_reactrouters/01_routerintro/src/App.js
@@ -5,14 +5,23 @@ import Contact from "./Contact";
 import About from "./About";
 
 // Importing route component
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
+{/*       This is not what we want to do.
+      <a href="/dog">Dog</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a> */}
+
+      <Link to="/about">About</Link>
+      <Link to="/dog">Dog</Link>
+      <Link to="/contact">Contact</Link>
+
       <Switch >
-        <Route exact path="/" component={Contact} />
+        <Route exact path="/contact" component={Contact} />
         <Route exact path="/dog" component={Dog} />
         <Route exact path="/about" component={About} />
       </Switch>
