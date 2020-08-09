3ecc4b631dd320ae2d948d6241e9f455406bd754
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-08-09 14:11:33 -0500

    activeClassName concepts.

--- 12_reactrouters/01_routerintro/src/App.js
+++ 12_reactrouters/01_routerintro/src/App.js
@@ -5,13 +5,13 @@ import Contact from "./Contact";
 import About from "./About";
 
 // Importing route component
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, Link, NavLink } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-{/*       This is not what we want to do.
+      {/*       This is not what we want to do.
       <a href="/dog">Dog</a>
       <a href="/about">About</a>
       <a href="/contact">Contact</a> */}
@@ -19,12 +19,16 @@ function App() {
       <Link to="/about">About</Link>
       <Link to="/dog">Dog</Link>
       <Link to="/contact">Contact</Link>
+      <br></br>
+        <NavLink exact activeClassName='active-link' to="/about">About</NavLink>
+      <NavLink exact activeClassName='active-link' to="/dog">Dog</NavLink>
+      <NavLink exact activeClassName='active-link' to="/contact">Contact</NavLink>
 
-      <Switch >
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/dog" component={Dog} />
-        <Route exact path="/about" component={About} />
-      </Switch>
+        <Switch >
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/dog" component={Dog} />
+          <Route exact path="/about" component={About} />
+        </Switch>
     </div>
   );
 }
