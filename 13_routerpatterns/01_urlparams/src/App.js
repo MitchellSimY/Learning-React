a318a5d28a64f03196bfe4dca1dadd460d9c5c03
MitchellSimY <Mitchellsimy@gmail.com>
2020-08-14 12:05:16 -0500

    Imported routes. Getting to work :flex:

--- 13_routerpatterns/01_urlparams/src/App.js
+++ 13_routerpatterns/01_urlparams/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Food from "./Food";
 
 // importing router.
-import {Route} from "react-router-dom";
+import { Route, Switch, Link, NavLink } from "react-router-dom";
 
 function App() {
   return (
