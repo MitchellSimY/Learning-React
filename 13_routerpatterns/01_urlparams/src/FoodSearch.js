87caf2480bb34844fc280420f87ac15358d1b84c
MitchellSimY <Mitchellsimy@gmail.com>
2020-08-14 13:30:27 -0500

    adding search methods.

---
+++ 13_routerpatterns/01_urlparams/src/FoodSearch.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class FoodSearch extends Component {
+    render() {
+        return (
+            <div> 
+                <input type="text" />
+            </div>
+        )
+    }
+}
+
+export default FoodSearch;
