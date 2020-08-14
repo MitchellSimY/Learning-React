f20a47cd389f92b23d58c0ff81600186442412ed
MitchellSimY <Mitchellsimy@gmail.com>
2020-08-14 12:36:31 -0500

    Meal updated.

--- 13_routerpatterns/01_urlparams/src/App.js
+++ 13_routerpatterns/01_urlparams/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Food from "./Food";
+import Meal from "./Meal";
 
 // importing router.
 import { Route, Switch, Link, NavLink } from "react-router-dom";
@@ -10,9 +11,12 @@ function App() {
     <div className="App">
 
 
+      {/* Using one parameter. */}
       <Route exact path='/food/:name'
-      render = {routeProps => <Food {...routeProps} />} />
+        render={routeProps => <Food {...routeProps} />} />
 
+      {/* How to use multiple parameters. */}
+      <Route exact path='/food/:foodName/drink/:drinkName' component={Meal} />
 
     </div>
   );
