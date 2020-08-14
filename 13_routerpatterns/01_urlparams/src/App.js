a4f3ec36f90a64c50d48fed1513b5f1a88d49db7
MitchellSimY <Mitchellsimy@gmail.com>
2020-08-14 13:27:38 -0500

    Home page added with error page.

--- 13_routerpatterns/01_urlparams/src/App.js
+++ 13_routerpatterns/01_urlparams/src/App.js
@@ -10,14 +10,21 @@ function App() {
   return (
     <div className="App">
 
+      <Switch>
+        {/* Using one parameter. */}
+        <Route exact path='/food/:name'
+          render={routeProps => <Food {...routeProps} />} />
 
-      {/* Using one parameter. */}
-      <Route exact path='/food/:name'
-        render={routeProps => <Food {...routeProps} />} />
+        {/* How to use multiple parameters. */}
+        <Route exact path='/food/:foodName/drink/:drinkName' component={Meal} />
 
-      {/* How to use multiple parameters. */}
-      <Route exact path='/food/:foodName/drink/:drinkName' component={Meal} />
+        {/* Home/Root route */}
+        <Route exact path="/" render={() => <h1>Home page.</h1>} />
 
+
+          {/* 404 page. */}
+        <Route exact render={() => <h1>Error not found.</h1>} />
+      </Switch>
     </div>
   );
 }
