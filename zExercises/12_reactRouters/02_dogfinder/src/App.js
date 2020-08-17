6d16d61eec8cc3d417e99adcc878cd59f4d120c3
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-08-17 08:34:28 -0500

    Made DogList component.

--- zExercises/12_reactRouters/02_dogfinder/src/App.js
+++ zExercises/12_reactRouters/02_dogfinder/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 // Importing components
 import NavBar from "./NavBar";
-
+import DogList from "./DogList";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <DogList />
       <h1 className="display-1">test</h1>
     </div>
   );
---
+++ zExercises/12_reactRouters/02_dogfinder/src/DogList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class DogList extends Component {
+    render() {
+        return (
+            <div>
+                Test from DogList
+            </div>
+        )
+    }
+}
+
+export default DogList;
