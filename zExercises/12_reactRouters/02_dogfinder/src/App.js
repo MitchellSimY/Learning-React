caa97375ca8b5730e9e57810bec7a5fa7cb5f5ee
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-08-17 08:31:48 -0500

    Made navbarJS

--- zExercises/12_reactRouters/02_dogfinder/src/App.js
+++ zExercises/12_reactRouters/02_dogfinder/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+// Importing components
+import NavBar from "./NavBar";
+
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1 className="display-1">test</h1>
     </div>
   );
