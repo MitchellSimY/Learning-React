fc8a53d1576614406d6347ba25e0475c7c06173c
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-08-18 15:37:13 -0500

    added comments for clarity.

--- zExercises/12_reactRouters/02_dogfinder/src/App.js
+++ zExercises/12_reactRouters/02_dogfinder/src/App.js
@@ -54,8 +54,14 @@ class App extends Component {
   }
   render() {
 
+    // Get dog is given the props. 
+    // This whole function prtty much says 
+    // Aight bro, I'll check the passed prop information
+    // then return a dog detail component.
     const getDog = props => {
+      // params name is given from the props.
       let name = props.match.params.name;
+
       let currentDog = this.props.dogs.find(
         dog => dog.name.toLowerCase() === name.toLowerCase()
       )
@@ -69,6 +75,7 @@ class App extends Component {
 
         <Switch>
           <Route exact path="/dogs" render={() => <DogList dogs={this.props.dogs} />} />
+          {/* render runs the function getDog */}
           <Route exact path="/dogs/:name" render={getDog} />
         </Switch>
       </div>
