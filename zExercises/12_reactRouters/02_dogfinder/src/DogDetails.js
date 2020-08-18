5f24ebffa4fefbab467382cfb5b89b7d41235577
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-08-18 15:15:19 -0500

    Dog details good. Just need to add Boostrap.

--- zExercises/12_reactRouters/02_dogfinder/src/DogDetails.js
+++ zExercises/12_reactRouters/02_dogfinder/src/DogDetails.js
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 // Importing CSS.
 import "./DogDetails.css";
 
+// importing routes.
+import {Link} from "react-router-dom"
+
 class DogDetails extends Component {
 
     render() {
@@ -12,15 +15,15 @@ class DogDetails extends Component {
             <div>
                 <div className="DogDetails">
 
-                    
+
                     <img src={dog.src} alt={dog.name} />
                     <h1>{dog.name}</h1>
                     <p>{dog.age} years old</p>
-                    {dog.facts.map( fact => (
+                    {dog.facts.map(fact => (
                         <p>{fact}</p>
                     ))}
 
-
+                        <Link to="/dogs">Go Back</Link>
                 </div>
 
 
